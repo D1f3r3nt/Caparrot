ac086859235c26256c6d0dbf6df258168e01b81e
D1f3r3nt <98466831+D1f3r3nt@users.noreply.github.com>
2023-02-06 18:59:02 +0100

    CP-26: Refactor Login
    Refactorizated login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -38,131 +38,177 @@ class _LoginPageState extends State<LoginPage> {
                 fit: BoxFit.cover,
                 opacity: 0.5),
           ),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: TextFormField(
-                    validator: (input) {
-                      if (input!.isEmpty) {
-                        return 'Perfavor posa un email';
-                      }
-                      return null;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    onSaved: (input) => _email = input!,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      filled: true,
-                      fillColor: Palete.white90,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: TextFormField(
-                    validator: (input) {
-                      if (input!.length < 6) {
-                        return 'La contrasenya ha de contenir un minim de 6 caracters';
-                      }
-                      return null;
-                    },
-                    onSaved: (input) => _password = input!,
-                    decoration: InputDecoration(
-                      labelText: 'Contrasenya',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      filled: true,
-                      fillColor: Palete.white90,
-                      focusColor: Palete.green90,
-                      hoverColor: Palete.green90,
-                    ),
-                    obscureText: true,
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Benvingut',
+                style: TextStyle(
+                    color: Palete.white20,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 8.0, horizontal: 30),
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Text(
-                          "Has olvidad la teva clau?",
-                          style: TextStyle(
-                            color: Palete.white20,
-                            fontWeight: FontWeight.bold,
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: TextFormField(
+                        validator: (input) {
+                          if (input!.isEmpty) {
+                            return 'Perfavor posa un email';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        onSaved: (input) => _email = input!,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          filled: true,
+                          fillColor: Palete.white90,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: TextFormField(
+                        validator: (input) {
+                          if (input!.length < 6) {
+                            return 'La contrasenya ha de contenir un minim de 6 caracters';
+                          }
+                          return null;
+                        },
+                        onSaved: (input) => _password = input!,
+                        decoration: InputDecoration(
+                          labelText: 'Contrasenya',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          filled: true,
+                          fillColor: Palete.white90,
+                          focusColor: Palete.green90,
+                          hoverColor: Palete.green90,
+                        ),
+                        obscureText: true,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 30),
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: Text(
+                              "Has olvidad la teva clau?",
+                              style: TextStyle(
+                                color: Palete.white20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
                         ),
+                      ],
+                    ),
+                    MaterialButton(
+                      color: Palete.black50,
+                      onPressed: _submit,
+                      child: Text(
+                        'Log In',
+                        style: TextStyle(color: Palete.white20, fontSize: 16),
                       ),
                     ),
                   ],
                 ),
-                MaterialButton(
+              ),
+              Divider(
+                color: Palete.white20,
+                indent: 10,
+                endIndent: 10,
+                thickness: 1,
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: MaterialButton(
                   color: Palete.black50,
-                  onPressed: _submit,
-                  child: Text(
-                    'Log In',
-                    style: TextStyle(color: Palete.white20),
-                  ),
-                ),
-                Divider(
-                  color: Palete.white20,
-                  indent: 10,
-                  endIndent: 10,
-                  thickness: 1,
-                ),
-                const SizedBox(height: 10),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: MaterialButton(
-                    color: Palete.black50,
-                    onPressed: _submit,
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          FontAwesomeIcons.google,
-                          color: Palete.white20,
-                        ),
-                        const SizedBox(width: 10),
-                        Text(
-                          'Log in with Google',
-                          style: TextStyle(color: Palete.white20, fontSize: 17),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                GestureDetector(
-                  child: Container(
-                    margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
-                    child: Text.rich(TextSpan(children: [
-                      TextSpan(text: "No tens cap compte? "),
-                      TextSpan(
-                        text: 'Registret',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).accentColor),
-                      ),
-                    ])),
-                  ),
-                  onTap: () {
-                    print('ok');
+                  onPressed: () {
+                    print('G');
                   },
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        FontAwesomeIcons.google,
+                        color: Palete.white20,
+                      ),
+                      const SizedBox(width: 10),
+                      Text(
+                        'Log in with Google',
+                        style: TextStyle(color: Palete.white20, fontSize: 17),
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 5),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: MaterialButton(
+                  color: Palete.black50,
+                  onPressed: () {
+                    print('A');
+                  },
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        FontAwesomeIcons.apple,
+                        color: Palete.white20,
+                      ),
+                      const SizedBox(width: 10),
+                      Text(
+                        'Log in with Apple',
+                        style: TextStyle(color: Palete.white20, fontSize: 17),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              GestureDetector(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "No tens cap compte? ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Registret',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Palete.white20,
+                      ),
+                    ),
+                  ],
+                ),
+                onTap: () =>
+                    Navigator.pushReplacementNamed(context, 'register'),
+              ),
+            ],
           ),
         ),
       ),
