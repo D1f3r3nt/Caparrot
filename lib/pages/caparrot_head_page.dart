f55226daa35d59ba80a6e1a32ffe3c04b722214d
JoanManelRamirezCuart <joanmanelramirez@paucasesnovescifp.cat>
2023-02-15 23:49:57 +0100

    CP-32
    3D-MODEL

---
+++ lib/pages/caparrot_head_page.dart
@@ -0,0 +1,46 @@
+import 'package:caparrot/utils/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
+
+class CapaHeadPage extends StatelessWidget {
+  const CapaHeadPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text("Grouxo"))),
+      body: Stack(children: <Widget>[
+        Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment(0.8, 1),
+              colors: [Colors.green, Colors.yellow],
+              tileMode: TileMode.mirror,
+            ),
+          ),
+        ),
+        RotatedBox(
+            quarterTurns: 1,
+            child: Container(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  'GROUXO',
+                  style: TextStyle(
+                      fontSize: 75.0, color: Color.fromRGBO(0, 0, 0, 0.368)),
+                ))),
+        ModelViewer(
+          backgroundColor: Color.fromRGBO(242, 236, 236, 0),
+          src: 'assets/ppp.glb', // a bundled asset file
+
+          ar: false,
+
+          autoRotate: true,
+          cameraControls: true,
+          disableZoom: true,
+          disablePan: true,
+        ),
+      ]),
+    );
+  }
+}
